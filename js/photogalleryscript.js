3edb04d7704195e4d9537b50db746232096efb54
keebmack <rmkebe@pointpark.edu>
2023-07-30 15:24:12 -0400

    adding photos

--- js/photogalleryscript.js
+++ js/photogalleryscript.js
@@ -1,23 +1,33 @@
 $(document).ready(function() {
     const lightbox = $('.lightbox');
     const lightboxImage = $('#lightbox-image');
+  
     const images = [
       'images/PointParkPioneersLogo.png',
       'images/PPUcampus1.jpg',
       'images/PPUcampus2.jpg',
     ];
   
-    $('.gallery-item').on('click', function() {
-      const index = $(this).index();
+    function openLightbox(index) {
       lightboxImage.attr('src', images[index]);
       lightbox.addClass('show');
+    }
+  
+    function closeLightbox() {
+      lightbox.removeClass('show');
+    }
+  
+    $('.gallery-item img').on('click', function() {
+      const index = $(this).data('index');
+      openLightbox(index);
     });
   
     lightbox.on('click', function() {
-      lightbox.removeClass('show');
+      closeLightbox();
     });
   
     lightboxImage.on('click', function(event) {
       event.stopPropagation();
     });
   });
+  
